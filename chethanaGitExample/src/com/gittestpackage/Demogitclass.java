212258e8c9bd4cc4925debdd05cc75662ecaf5ee
cs84136 <cs84136@INBASDPC06364.TechMahindra.com>
2015-05-04 15:41:14 +0530

    first commit

---
+++ chethanaGitExample/src/com/gittestpackage/Demogitclass.java
@@ -0,0 +1,14 @@
+package com.gittestpackage;
+
+public class Demogitclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World\n");
+System.out.println("bye");
+	}
+
+}
